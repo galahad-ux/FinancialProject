0fc38d453914309bc167d87066e733ff339e79d3
ulak-github <aayushkaulak@gmail.com>
2026-01-10 09:47:30 +0100

    User class
    Added simple authentication methods and storage for login window

--- src/main/java/org/isep/financialproject/User.java
+++ src/main/java/org/isep/financialproject/User.java
@@ -1,24 +1,147 @@
 package org.isep.financialproject;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    private final List<Portfolio> portfolios = new ArrayList<>();
+    public String userFullName;
+    public String userEmail;
+    public String username;
+    public String userPassword;
+    public Currency preferredCurrency;
+    public static List<User> allUsers = null;
+    private final List<Portfolio> portfolios;
 
+    public User(String userFullName, String userEmail,String username, String userPassword, Currency preferredCurrency){
+        this.userFullName = userFullName;
+        this.userEmail = userEmail;
+        this.username = username;
+        this.userPassword = userPassword;
+        this.preferredCurrency = preferredCurrency;
+        this.portfolios = new ArrayList<>();
+    }
+
+    //Getters
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public Currency getPreferredCurrency() {
+        return preferredCurrency;
+    }
+
+    public void setPreferredCurrency(Currency preferredCurrency) {
+        this.preferredCurrency = preferredCurrency;
+    }
+
+    public List<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    //Portfolio management methods
     public void addPortfolio(Portfolio portfolio) {
         portfolios.add(portfolio);
     }
 
-    public List<Portfolio> getPortfolios() {
-        return List.copyOf(portfolios); // protects internal list
+    public void removePortfolio (String portfolioName){
+
     }
 
-    public double getNetWorth() {
-        double total = 0;
-        for (Portfolio p : portfolios) {
-            total += p.getValue();
-        }
-        return total;
+    public void getPortfolio(String portfolioName){
+
     }
+
+    //Authentication management methods
+    public static User checkUsername(String username){
+        for (User user : allUsers){
+            if(user.getUsername().equalsIgnoreCase(username)){
+                return user;
+            }
+        }
+        return null;
+    }
+    public static boolean emailExists(String username){
+        return checkUsername(username) != null;
+    }
+    public static boolean register (String userFullName, String userEmail, String username, String userPassword, Currency preferredCurrency){
+        if (emailExists(userEmail)){
+            System.out.println("User email already exists");
+            return false;
+        }
+        User newUser = new User(userFullName,userEmail,username,userPassword, preferredCurrency);
+        allUsers.add(newUser);
+        User.saveAllUsers(allUsers, "users.csv");
+        return true;
+    }
+
+    public static boolean authenticate(String username, String userPassword){
+        User user = checkUsername(username);
+        if (user != null && user.getUserPassword().equals(userPassword)){
+            return true;
+        }
+        return false;
+    }
+
+
+    //CSV methods
+    public String toCSV(){
+        return userFullName + "," + userEmail + "," + username + "," + userPassword + "," + preferredCurrency;
+    }
+
+    public static User fromCSV(String csvLine){
+        String[] objects = csvLine.split(",");
+        return new User(objects[0], objects[1], objects[2], objects[3],);
+    }
+
+    //saving new user to csv file
+    public static void saveAllUsers(List<User> users, String filepath){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("users.csv"))){
+            for (User a : users){
+                writer.write(a.toCSV());
+                writer.newLine();
+            }
+            System.out.println("Saved to file");
+        } catch (IOException e) {
+            System.out.println("Error saving users" + e.getMessage());
+        }
+    }
+
+    public static List<User> loadAllUsers(String filePath){
+        List<User> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("users.csv"))){
+            String line;
+            while((line = reader.readLine()) != null){
+                if (!line.trim().isEmpty()){
+                    users.add(fromCSV(line));
+                }
+            }
+            System.out.println("Users loaded");
+        }catch (IOException e){
+            System.out.println("Error loading users" + e.getMessage());
+        }
+        return users;
+    }
+
+
+
+//    public double getNetWorth() {
+//        double total = 0;
+//        for (Portfolio p : portfolios) {
+//            total += p.getValue();
+//        }
+//        return total;
+//    }
 }
