459b03778e73c78a47e84418dc2f05512d7640a5
galahad-ux <milla.vien@gmail.com>
2026-01-12 23:17:19 +0100

    updated Portfolio

--- src/main/java/org/isep/financialproject/Portfolio.java
+++ src/main/java/org/isep/financialproject/Portfolio.java
@@ -1,15 +1,32 @@
 package org.isep.financialproject;
 
+import java.util.Date;
+import java.util.Objects;
+
 public abstract class Portfolio {
     protected String name;
+    protected String description;
+    protected Currency refCurrency;
+    protected Date creationDate;
 
-    public Portfolio(String name) {
-        this.name = name;
+    protected Portfolio(String name, String description, Currency refCurrency, Date creationDate) {
+        this.name = Objects.requireNonNull(name);
+        this.description = description;
+        this.refCurrency = Objects.requireNonNull(refCurrency);
+        this.creationDate = new Date(Objects.requireNonNull(creationDate).getTime());
     }
 
     public String getName() {
         return name;
     }
-    
+
+    public Currency getRefCurrency() {
+        return refCurrency;
+    }
+
+    public Date getCreationDate() {
+        return new Date(creationDate.getTime());
+    }
+
     public abstract double getValue();
 }
