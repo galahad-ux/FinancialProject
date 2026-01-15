bacf0b9af3c49915d31ab1652900f64b88f8ae51
galahad-ux <milla.vien@gmail.com>
2026-01-15 18:23:24 +0100

    Created CreateChecking + controller

---
+++ src/main/java/org/isep/financialproject/CreateCheckingController.java
@@ -0,0 +1,49 @@
+package org.isep.financialproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CreateCheckingController {
+    @FXML private TextField nameField;
+    @FXML private TextField descriptionField;
+    @FXML private TextField accNumField;
+    @FXML private TextField initialAmountField;
+    @FXML private TextField withdrawLimitField;
+    @FXML private TextField spendLimitField;
+    @FXML private Label messageLabel;
+
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @FXML
+    private void handleCreate(ActionEvent event) {
+        try {
+            if (user == null) {
+                messageLabel.setText("Error: no logged-in user.");
+                return;
+            }
+
+            String name = nameField.getText();
+            String description = descriptionField.getText();
+            String accNum = accNumField.getText();
+
+            double initialAmount = Double.parseDouble(initialAmountField.getText());
+            double withdrawLimit = Double.parseDouble(withdrawLimitField.getText());
+            double spendLimit = Double.parseDouble(spendLimitField.getText());
+
+            user.createCheckingAccount(name, description, accNum, initialAmount, withdrawLimit, spendLimit);
+
+            messageLabel.setText("Checking account created!");
+
+        } catch (NumberFormatException e) {
+            messageLabel.setText("Please enter valid numbers.");
+        } catch (Exception e) {
+            messageLabel.setText("Error: " + e.getMessage());
+        }
+    }
+}
