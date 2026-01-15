42534455f3db895d6851abed71fb4b69339dadf8
Yueliang135 <Ashleyhanshanghai@163.com>
2026-01-16 06:58:02 +0800

    a

--- src/main/java/org/isep/financialproject/DashboardViewController.java
+++ src/main/java/org/isep/financialproject/DashboardViewController.java
@@ -6,13 +6,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import java.io.IOException;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class DashboardViewController {
     @FXML
     private Button APButton;
+    @FXML
+    private Button goToInvestment;
 
-
+    @FXML
+    private Button goToBank;
 
     public void openAPV() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddPortfolio.fxml"));
@@ -26,6 +36,22 @@ public class DashboardViewController {
         stage.setScene(new Scene(root));
         stage.show();
 
+
+    }
+    @FXML
+    private void goToInvestmentPage() throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Investment-view.fxml"));
+        Parent root = loader.load();
+        goToInvestment.getScene().setRoot(root);
+
+    }
+
+    @FXML
+    private void goToBankPage() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Bank-view.fxml"));
+        Parent root = loader.load();
+        goToInvestment.getScene().setRoot(root);
 
     }
 
